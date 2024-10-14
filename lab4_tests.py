e61d4f29d32809d1bffbb55619ce938a5d61b8e4
anrathi-calpoly <anrathi@calpoly.edu>
2024-10-13 21:58:48 -0700

    Create lab4_tests.py

---
+++ lab4_tests.py
@@ -0,0 +1,40 @@
+import data
+import lab4
+import unittest
+
+
+# Write your test cases for each part below.
+
+class TestCases(unittest.TestCase):
+    # Part 1
+    def test_first_element_1(self):
+        input = [[1,2], [3,4]]
+        result = lab4.first_element(input)
+        expected = [1, 3]
+        self.assertEqual(expected, result)
+
+
+    def test_first_element_2(self):
+        # write a second test here
+
+
+    # Part 2
+
+
+    # Part 3
+
+
+    # Part 4
+
+
+    # Part 5
+
+
+    # Part 6
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
